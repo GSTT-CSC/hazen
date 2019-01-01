b728e013347f680a48dd62a8c2fa21ca69a1304e
Haris Shuaib <mohammad_haris.shuaib@kcl.ac.uk>
2019-01-01 20:17:55 +0000

    Updated uniformity measurement and test.

--- hazenlib/uniformity.py
+++ hazenlib/uniformity.py
@@ -17,66 +17,29 @@ neil.heraghty@nhs.net
 14/05/2018
 
 """
-import cv2 as cv
+import json
+import sys
+
 import numpy as np
 import matplotlib.pyplot as plt
-from pydicom.filereader import dcmread
+import pydicom
 
-
-def find_circle(a):
-    """
-    Finds a circle that fits the outline of the phantom using the Hough Transform
-
-    parameters:
-    ---------------
-    a: image array (should be uint8)
-
-    returns:
-    ---------------
-    cenx, ceny: xy pixel coordinates of the centre of the phantom
-    cradius : radius of the phantom in pixels
-    errCircle: error message if good circle isn't found
-    """
-
-    # Perform Hough transform to find circle
-    circles = cv.HoughCircles(a, cv.HOUGH_GRADIENT, 1, 200, param1=30,
-                              param2=45, minRadius=0, maxRadius=0)
-
-    # Check that a single phantom was found
-    if len(circles) == 1:
-        pass
-        # errCircle = "1 circle found."
-    else:
-        errCircle = "Wrong number of circles detected, check image."
-        print(errCircle)
-
-    # Draw circle onto original image
-    circles = np.uint16(np.around(circles))
-    for i in circles[0, :]:
-        # draw the outer circle
-        cv.circle(a, (i[0], i[1]), i[2], 128, 2)
-        # draw the center of the circle
-        cv.circle(a, (i[0], i[1]), 2, 128, 2)
-
-    cenx = i[0]
-    ceny = i[1]
-    cradius = i[2]
-    return (cenx, ceny, cradius)
+import hazenlib as hazen
 
 
 def mode(a, axis=0):
     """
-        Finds the modal value of an array. From scipy.stats.mode
+    Finds the modal value of an array. From scipy.stats.mode
 
-        parameters:
-        ---------------
-        a: array
+    Parameters:
+    ---------------
+    a: array
 
-        returns:
-        ---------------
-        mostfrequent: the modal value
-        oldcounts: the number of times this value was counted (check this)
-        """
+    Returns:
+    ---------------
+    mostfrequent: the modal value
+    oldcounts: the number of times this value was counted (check this)
+    """
 
     scores = np.unique(np.ravel(a))       # get ALL unique values
     testshape = list(a.shape)
@@ -94,27 +57,39 @@ def mode(a, axis=0):
     return mostfrequent, oldcounts
 
 
-def main(image):
-    # Read DICOM image
-    #image = dcmread('uniformCNSA.dcm')     # Read the DICOM file
+def get_fractional_uniformity():
+    pass
 
-    # Prepare image for processing
-    idata = image.pixel_array              # Read the pixel values into an array
-    idata = np.array(idata)                # Make it a numpy array
-    idown = ((idata / idata.max()) * 256).astype('uint8')  # Downscale to uint8 for openCV techniques
+
+def get_ghosting():
+    pass
+
+
+def get_distortion():
+    pass
+
+
+def main(data: list) -> dict:
+
+    if len(data) != 1:
+        raise Exception('Only single DICOM input.')
+
+    image = pydicom.read_file(data[0])
+
+    arr = image.pixel_array
 
     # Find phantom centre and radius
-    (cenx, ceny, cradius) = find_circle(idown)
+    cenx, ceny, cradius = hazen.find_circle(image)
 
     # Create central 10x10 ROI and measure modal value
-    cenroi = idata[(ceny-5):(ceny+5),(cenx-5):(cenx+5)]
+    cenroi = arr[(ceny-5):(ceny+5), (cenx-5):(cenx+5)]
     cenroi_flat = cenroi.flatten()
     (cenmode, modenum) = mode(cenroi_flat)
 
     # Create 160-pixel profiles (horizontal and vertical, centred at cenx,ceny)
-    horroi = idata[(ceny-5):(ceny+5),(cenx-80):(cenx+80)]
+    horroi = arr[(ceny-5):(ceny+5), (cenx-80):(cenx+80)]
     horprof = np.mean(horroi, axis=0)
-    verroi = idata[(ceny-80):(ceny+80),(cenx-5):(cenx+5)]
+    verroi = arr[(ceny-80):(ceny+80), (cenx-5):(cenx+5)]
     verprof = np.mean(verroi, axis=1)
 
     # Count how many elements are within 0.9-1.1 times the modal value
@@ -127,48 +102,40 @@ def main(image):
     fract_uniformity_hor = hornum/160
     fract_uniformity_ver = vernum/160
 
-    #print("Horizontal Fractional Uniformity: ",np.round(fract_uniformity_hor,2))
-    #print("Vertical Fractional Uniformity: ",np.round(fract_uniformity_ver,2))
-
     # Define 75% area mask for alternative uniformity measures
     r = cradius*0.865
-    n = len(idata)
+    n = len(arr)
     y,x = np.ogrid[:n,:n]
     dist_from_center = np.sqrt((x - cenx)**2 + (y-ceny)**2)
 
     mask = dist_from_center <= r
 
     # Calculate stats for masked region
-    roimean = np.mean(idata[mask])
-    roistd = np.std(idata[mask])
-    roimax = np.amax(idata[mask])
-    roimin = np.amin(idata[mask])
+    roimean = np.mean(arr[mask])
+    roistd = np.std(arr[mask])
+    roimax = np.amax(arr[mask])
+    roimin = np.amin(arr[mask])
 
     # Calculate CoV and integral uniformity
     cov = 100*roistd/roimean
     intuniform = (1-(roimax-roimin)/(roimax+roimin))*100
 
-    #print("CoV: ",np.round(cov,1),"%")
-    #print("Integral Uniformity (ACR): ",np.round(intuniform,1),"%")
-
     # ------ Ghosting ------
 
     # Measure mean pixel value within rectangular ROIs slightly outside the top/bottom/left/right of phantom
-    meantop = np.mean(idata[(cenx-50):(cenx+50),(ceny-cradius-20):(ceny-cradius-10)])
-    meanbot = np.mean(idata[(cenx-50):(cenx+50),(ceny+cradius+10):(ceny+cradius+20)])
-    meanl = np.mean(idata[(cenx-cradius-20):(cenx-cradius-10),(ceny-50):(ceny+50)])
-    meanr = np.mean(idata[(cenx+cradius+10):(cenx+cradius+20),(ceny-50):(ceny+50)])
+    meantop = np.mean(arr[(cenx-50):(cenx+50),(ceny-cradius-20):(ceny-cradius-10)])
+    meanbot = np.mean(arr[(cenx-50):(cenx+50),(ceny+cradius+10):(ceny+cradius+20)])
+    meanl = np.mean(arr[(cenx-cradius-20):(cenx-cradius-10),(ceny-50):(ceny+50)])
+    meanr = np.mean(arr[(cenx+cradius+10):(cenx+cradius+20),(ceny-50):(ceny+50)])
 
     # Calculate percentage ghosting
     ghosting = np.abs(((meantop+meanbot)-(meanl+meanr))/(2*roimean))*100
 
-    #print("Ghosting: ",np.round(ghosting,1), "%")
-
     # ------ Distortion ------
 
     # Threshold using half of roimin as a cutoff
-    thresh = idata < roimin/2
-    ithresh = idata
+    thresh = arr < roimin/2
+    ithresh = arr
     ithresh[thresh] = 0
 
     # Find the indices of thresholded pixels
@@ -178,6 +145,24 @@ def main(image):
     idistort = (bbxstop-bbxstart)/(bbystop-bbystart)
     idistort =np.abs(idistort-1)
 
-    #print("Distortion: ", np.round(idistort,2),"%")
+    results = {
 
-    return fract_uniformity_hor, fract_uniformity_ver, cov, intuniform, ghosting, idistort
+        'uniformity':
+            {
+                'horizontal':
+                    {
+                        'IPEM': fract_uniformity_hor
+                    },
+                'vertical':
+                    {
+                        'IPEM': fract_uniformity_ver
+                    }
+
+            }
+    }
+
+    return json.dumps(results, indent=4)
+
+
+if __name__ == "__main__":
+    main([sys.argv[1]])
---
+++ tests/test_uniformity.py
@@ -0,0 +1,23 @@
+import unittest
+import pathlib
+import json
+
+import pytest
+import pydicom
+
+import hazenlib.uniformity as hazen_uniformity
+from tests import TEST_DATA_DIR
+
+
+class TestSlicePosition(unittest.TestCase):
+
+    UNIFORMITY_DATA = pathlib.Path(TEST_DATA_DIR / 'uniformity')
+
+    def setUp(self):
+        self.test_file = [str(self.UNIFORMITY_DATA / 'axial_oil.IMA')]
+
+    def test_uniformity(self):
+        results = json.loads(hazen_uniformity.main(self.test_file))
+
+        assert results['uniformity']['horizontal']['IPEM'] == 1.0
+        assert results['uniformity']['vertical']['IPEM'] == 0.98125
