2438a7610874ca667a6b9ef2f514b8672404d0c6
Haris Shuaib <mohammad_haris.shuaib@kcl.ac.uk>
2019-12-18 13:29:46 +0000

    Sonar wasn't working because they have had change in the properties file, now fixed hopefully. Added getting signal and background rois.

--- hazenlib/ghosting.py
+++ hazenlib/ghosting.py
@@ -38,13 +38,12 @@ def calculate_ghost_intensity(ghost, phantom, noise) -> float:
     if phantom_mean < ghost_mean or phantom_mean < noise_mean:
         raise Exception(f"The mean phantom signal is lower than the ghost or the noise signal. This can't be the case ")
 
-    return 100*(ghost_mean-noise_mean)/phantom_mean
+    return 100 * (ghost_mean - noise_mean) / phantom_mean
 
 
 def get_signal_bounding_box(array: np.ndarray):
-
     max_signal = np.max(array)
-    signal_limit = max_signal*0.5  # assumes phantom signal is at least 50% of the max signal inside the phantom
+    signal_limit = max_signal * 0.5  # assumes phantom signal is at least 50% of the max signal inside the phantom
     signal = []
     for idx, voxel in np.ndenumerate(array):
         if voxel > signal_limit:
@@ -61,13 +60,61 @@ def get_signal_bounding_box(array: np.ndarray):
     return upper_row, lower_row, left_row, right_row
 
 
-def get_ghosting(dicom_data: list) -> dict:
+def get_signal_slice(bounding_box, slice_size=10):
+    slice_radius = round(slice_size / 2)
+    upper_row, lower_row, left_column, right_column = bounding_box
+    centre_row = upper_row + round((lower_row - upper_row) / 2)
+    centre_column = left_column + round((right_column - left_column) / 2)
 
+    idxs = (np.array(range(centre_row - slice_radius, centre_row + slice_radius), dtype=np.intp)[:, np.newaxis],
+            np.array(range(centre_column - slice_radius, centre_column + slice_radius), dtype=np.intp))
+    return idxs
+
+
+def get_pe_direction(dcm):
+    return dcm.InPlanePhaseEncodingDirection
+
+
+def get_background_rois(dcm, signal_centre):
+    background_rois = []
+
+    if get_pe_direction(dcm) == 'ROW':
+        # phase encoding is left -right i.e. increases with columns
+        if signal_centre[0] < dcm.Rows * 0.5:
+            # phantom is in top half of image
+            background_rois_row = round(dcm.Rows * 0.75)  # in the bottom quadrant
+        else:
+            # phantom is bottom half of image
+            background_rois_row = round(dcm.Rows * 0.25)  # in the top quadrant
+        background_rois.append((background_rois_row, signal_centre[1]))
+
+        if signal_centre[1] >= round(dcm.Columns/2):
+            # phantom is right half of image need 3 ROIs evenly spaced from 0->background_roi[0]
+            gap = round(background_rois[0][1]/ 4)
+            background_rois = [(background_rois_row, background_rois[0][1] - i * gap) for i in range(4)]
+        else:
+            # phantom is left half of image need 3 ROIs evenly spaced from background_roi[0]->end
+            gap = round((dcm.Columns - background_rois[0][1]) / 4)
+            background_rois = [(background_rois_row, background_rois[0][1] - i * gap) for i in range(4)]
+
+    else:
+        if signal_centre[1] < dcm.Columns * 0.5:
+            # phantom is in left half of image
+            background_rois_column = round(dcm.Columns * 0.75)  # in the right quadrant
+        else:
+            # phantom is right half of image
+            background_rois_column = round(dcm.Columns * 0.25)  # in the top quadrant
+        background_rois.append((signal_centre[0], background_rois_column))
+
+
+    return background_rois
+
+
+def get_ghosting(dicom_data: list) -> dict:
     return {}
 
 
 def main(data: list) -> dict:
-
     data = [pydicom.read_file(dcm) for dcm in data]  # load dicom objects into memory
 
     results = get_ghosting(data)
--- tests/test_ghosting.py
+++ tests/test_ghosting.py
@@ -4,12 +4,22 @@ import pathlib
 import numpy as np
 import pydicom
 import pytest
+import matplotlib
+# matplotlib.use("agg")
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
 
 import hazenlib.ghosting as hazen_ghosting
 from tests import TEST_DATA_DIR
 
 
 class TestSliceWidth(unittest.TestCase):
+    SIGNAL_BOUNDING_BOX = (242, 325, 251, 335)
+    SIGNAL_CENTRE = [284, 293]
+    SIGNAL_SLICE = np.array(range(284-5, 284+5), dtype=np.intp)[:, np.newaxis], np.array(range(293-5, 293+5), dtype=np.intp)
+    BACKGROUND_ROIS = [(88, 293), (88, 220), (88, 147), (88, 74)]
+
     def setUp(self):
         self.dcm = pydicom.read_file(str(TEST_DATA_DIR / 'ghosting' / 'GHOSTING' / 'IM_0001.dcm'))
 
@@ -34,5 +44,41 @@ class TestSliceWidth(unittest.TestCase):
                                                                 noise=np.asarray([10]))
 
     def test_get_signal_bounding_box(self):
-        (upper_row, lower_row, left_column, right_columns) = hazen_ghosting.get_signal_bounding_box(self.dcm.pixel_array)
-        assert (upper_row, lower_row, left_column, right_columns) == (242, 325, 251, 335)
+        (upper_row, lower_row, left_column, right_column) = hazen_ghosting.get_signal_bounding_box(self.dcm.pixel_array)
+
+        # Create figure and axes
+        fig, ax = plt.subplots(1)
+
+        # Display the image
+        ax.imshow(self.dcm.pixel_array)
+        rect = patches.Rectangle((left_column, upper_row),
+                                 right_column-left_column, lower_row-upper_row,
+                                 linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+        rect = patches.Rectangle((288, 279), 10, 10, linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+        plt.show()
+
+        assert (upper_row, lower_row, left_column, right_column) == self.SIGNAL_BOUNDING_BOX
+
+    def test_get_signal_slice(self):
+        assert list(hazen_ghosting.get_signal_slice(self.SIGNAL_BOUNDING_BOX)[0]) == list(self.SIGNAL_SLICE[0])
+        assert list(hazen_ghosting.get_signal_slice(self.SIGNAL_BOUNDING_BOX)[1]) == list(self.SIGNAL_SLICE[1])
+
+    def test_get_pe_direction(self):
+        assert hazen_ghosting.get_pe_direction(self.dcm) == 'ROW'
+
+    def test_get_background_rois(self):
+        # # Create figure and axes
+        # fig, ax = plt.subplots(1)
+        #
+        # # Display the image
+        # ax.imshow(self.dcm.pixel_array)
+        #
+        # for roi in self.BACKGROUND_ROIS:
+        #
+        #     rect = patches.Rectangle((roi[1]-5, roi[0]-5), 10, 10, linewidth=1, edgecolor='r', facecolor='none')
+        #     ax.add_patch(rect)
+        #
+        # plt.show()
+        assert hazen_ghosting.get_background_rois(self.dcm, self.SIGNAL_CENTRE) == self.BACKGROUND_ROIS
