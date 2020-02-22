0b3308d9cc4921f06ec99d235210a6be23f55277
Haris Shuaib <mohammad_haris.shuaib@kcl.ac.uk>
2020-02-22 19:06:58 +0000

    Add plot to both snr methods
    See: #24

--- tests/test_snr.py
+++ tests/test_snr.py
@@ -11,10 +11,15 @@ from tests import TEST_DATA_DIR
 class TestSnr(unittest.TestCase):
 
     SNR_DATA = pathlib.Path(TEST_DATA_DIR / 'snr')
+    ORIENTATION = 'Transverse'
+    OBJECT_CENTRE = (131, 122)
 
     def setUp(self):
-        self.test_file = pydicom.read_file(str(self.SNR_DATA / 'uniform-circle.IMA'), force=True)
+        self.test_file = pydicom.read_file(str(self.SNR_DATA / 'tra_250_2meas_1.IMA'), force=True)
+
+    def test_get_object_centre(self):
+        assert hazenlib.snr.get_object_centre(self.test_file) == self.OBJECT_CENTRE
 
     def test_image_snr(self):
         val = hazenlib.snr.main(data=[self.test_file])
-        assert val["normalised_snr_smoothing_method_0"] == 2509.1319217231753
+        assert val["seFoV250_2meas_slice5mm_tra_repeat_PSN_noDC_2_1_normalised_snr_smoothing"] == 2509.1319217231753
