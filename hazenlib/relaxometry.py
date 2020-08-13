d610445d14d4a8945f66b97e9f2abc02b9ae4d38
Paul Wilson <paulc.wilson@belfasttrust.hscni.net>
2020-08-13 14:19:09 +0100

    HZN-77 Generate sample ROIs after image fit and order images
    Pixel values for each sphere are needed for each image in the stack, and
    images need sorted by either TI (for T1 relaxometry) or TE (for T2).
    
    This patch:
     * Transforms the coordinates of the centre of each sphere (points of
       interest, POIs) based on warp_matrix transformation
     * Generates ROI masks by convolving with a sampling kernel
     * Orders images in stack by TI or TE
     * Moves unittest parameters to top of class definition

--- hazenlib/relaxometry.py
+++ hazenlib/relaxometry.py
@@ -47,14 +47,39 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import skimage.morphology
+import scipy.ndimage
 
-#import hazenlib
+# import hazenlib
+
+def outline_mask(im):
+    """Create contour lines to outline pixels."""
+    # Adapted from https://stackoverflow.com/questions/40892203/can-matplotlib-contours-match-pixel-edges
+    lines = []
+    pad = np.pad(im, [(1, 1), (1, 1)])  # zero padding
+
+    im0 = np.abs(np.diff(pad, n=1, axis=0))[:, 1:]
+    im1 = np.abs(np.diff(pad, n=1, axis=1))[1:, :]
+
+    im0 = np.diff(im0, n=1, axis=1)
+    starts = np.argwhere(im0 == 1)
+    ends = np.argwhere(im0 == -1)
+    lines += [([s[0]-.5, s[0]-.5], [s[1]+.5, e[1]+.5]) for s, e
+              in zip(starts, ends)]
+
+    im1 = np.diff(im1, n=1, axis=0).T
+    starts = np.argwhere(im1 == 1)
+    ends = np.argwhere(im1 == -1)
+    lines += [([s[1]+.5, e[1]+.5], [s[0]-.5, s[0]-.5]) for s, e
+              in zip(starts, ends)]
+
+    return lines
 
 def transform_coords(coords, rt_matrix, input_yx=True, output_yx=True):
     """
     Convert coordinates using RT transformation matrix.
-    
-    Note that arrays containing pixel information as displayed using 
+
+    Note that arrays containing pixel information as displayed using
     plt.imshow(pixel_array), for example are referenced using the yx notation,
     e.g. pixel_array[y,x]. Plotting points or patches using matplotlib requires
     xy notation, e.g. plt.scatter(x,y). The correct input and output notation
@@ -65,10 +90,10 @@ def transform_coords(coords, rt_matrix, input_yx=True, output_yx=True):
     coords : np.array or tuple
         Array (n,2) of coordinates to transform.
     rt_matrix : np.array
-        Array (2,3) of transform matrix (Rotation and Translation). See e.g. 
+        Array (2,3) of transform matrix (Rotation and Translation). See e.g.
         cv2.transform() for details.
     input_yx : bool, optional
-        Select the input coordinate format.
+        Select the input coordinate format relative to the image.
         If True, input array has y-coordinate first, i.e.:
             [[y1,x1],
              [y2,x2],
@@ -90,30 +115,31 @@ def transform_coords(coords, rt_matrix, input_yx=True, output_yx=True):
         Returns (n,2) array of transformed coordinates.
 
     """
-    in_coords = np.array(coords) # ensure using np array
-    
-    if input_yx: # convert to xy format
+    in_coords = np.array(coords)  # ensure using np array
+
+    if input_yx:  # convert to xy format
         in_coords = np.flip(in_coords, axis=1)
-    
+
     out_coords = cv.transform(np.array([in_coords]), rt_matrix)
-    out_coords = out_coords[0] # reduce to two dimensions
-    
+    out_coords = out_coords[0]  # reduce to two dimensions
+
     if output_yx:
         out_coords = np.flip(out_coords, axis=1)
-            
-    return out_coords
 
+    return out_coords
 
 
 class ImageStack():
     """Object to hold image_slices and methods for T1, T2 calculation."""
-    
+
     # TODO define in subclasses
     T1_sphere_centres = []
     T1_bolt_centres = []
-
     
-    def __init__(self, image_slices, template_px, plate_number=None):
+    SAMPLE_ELEMENT = skimage.morphology.square(5)
+
+    def __init__(self, image_slices, template_px, plate_number=None,
+                 dicom_order_key=None):
         """
         Create ImageStack object.
 
@@ -121,11 +147,14 @@ class ImageStack():
         ----------
         image_slices : list of pydicom.FileDataSet objects
             List of pydicom objects to perform relaxometry analysis on.
-        template_px : numpy array
+        template_px : numpy array (or None)
             Pixel array of template image for ROI location.
         plate_number : int {3,4,5}, optional
             For future use. Reference to the plate in the relaxometry phantom.
             The default is None.
+        dicom_order_key : string, optional
+            DICOM attribute to order images. Typically 'InversionTime' for T1
+            relaxometry or 'EchoTime' for T2.
 
         Returns
         -------
@@ -134,23 +163,21 @@ class ImageStack():
         """
         # load template
         self.template_px = template_px
-        
-        self.images = image_slices # store images
-        # need to sort by TE or TI
 
+        self.images = image_slices  # store images
 
     def template_fit(self, image_index=0):
         """
         Calculate RT transformation matrix to fit template to image.
-        
+
         The template pixel array, self.template_px, is fitted to one of the
         images in self.images (default=0). The resultant RT matrix is stored as
         self.warp_matrix.
-        
+
         This matrix can be used to map coordinates from template space to image
-        space using transform_coords(...), or to map masks from template space 
+        space using transform_coords(...), or to map masks from template space
         to image space using cv2.warpAffine(...).
-        
+
         To map from image space to template space, the 'inverse' RT matrix can
         be calculated using:
           inverse_transform_matrix = cv.invertAffineTransform(self.warp_matrix)
@@ -164,65 +191,64 @@ class ImageStack():
         -------
         warp_matrix : np.array
             RT transform matrix (2,3).
-            
+
         Further details
         ---------------
-        
-            
+
         TODO
         ----
-        This routine is quite slow. It may be possible to extract the bolt
-        hole locations and fit from them, or to optomise fit parameters.
-        Running an edge-detection algorithm may also improve results as pixel
-        values are highly variable.
-        
+        This routine is suboptimal. It may be better to extract the bolt
+        hole locations and fit from them, or run an edge-detection algorithm
+        as pixel values are highly variable between scanners and manufacturers.
+
         Need to check if image is real valued, typically signed then shifted so
-        background is 2048, or magnitude image. Currently it assumes magnitude 
+        background is 2048, or magnitude image. Currently it assumes magnitude
         image.
 
         """
-        #template_px = self.template.pixel_array
-        target_px = self.images[0].pixel_array # TODO ensure list is sorted
-        
-        #TODO Check if need to subtract 2048
-        self.template8bit = cv.normalize(abs(self.template_px.astype(np.int, casting='safe')-2048),
-                             None, 0, 255, norm_type=cv.NORM_MINMAX,
-                             dtype=cv.CV_8U)
-        
-        self.target8bit = cv.normalize(target_px, 
-                                  None, 0, 255, norm_type=cv.NORM_MINMAX,
-                                  dtype=cv.CV_8U)
+        target_px = self.images[0].pixel_array  # TODO ensure list is sorted
 
-        
-        
+        #  TODO Check if need to subtract 2048
+        self.template8bit = \
+            cv.normalize(abs(self.template_px.astype(np.int,
+                                                     casting='safe')-2048),
+                         None, 0, 255, norm_type=cv.NORM_MINMAX,
+                         dtype=cv.CV_8U)
+
+        self.target8bit = cv.normalize(target_px,
+                                       None, 0, 255, norm_type=cv.NORM_MINMAX,
+                                       dtype=cv.CV_8U)
+
         # initialise transofrmation fitting parameters.
-        #TODO optomise parameters for time saving
-        number_of_iterations = 500;
-        termination_eps = 1e-10;
+        # TODO optomise parameters for time saving
+        number_of_iterations = 500
+        termination_eps = 1e-10
         criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT,
                     number_of_iterations,  termination_eps)
-        self.warp_matrix=np.eye(2,3,dtype=np.float32)
-        
-        # Apply transformation
-        self.template_cc, self.warp_matrix = cv.findTransformECC(self.template8bit, self.target8bit, self.warp_matrix, criteria=criteria)
-    
-        self.warped_template8bit = cv.warpAffine(self.template8bit, self.warp_matrix, 
-                                     (self.template8bit.shape[1],
-                                      self.template8bit.shape[0]))
-    
-        return self.warp_matrix
+        self.warp_matrix = np.eye(2, 3, dtype=np.float32)
 
+        # Apply transformation
+        self.template_cc, self.warp_matrix = \
+            cv.findTransformECC(self.template8bit, self.target8bit,
+                                self.warp_matrix, criteria=criteria)
+
+        self.warped_template8bit = cv.warpAffine(self.template8bit,
+                                                 self.warp_matrix,
+                                                 (self.template8bit.shape[1],
+                                                  self.template8bit.shape[0]))
+
+        return self.warp_matrix
 
     def plot_fit(self):
         """
         Visual representation of target fitting.
-        
+
         Create 2x2 subplot showing 8-bit version of:
             1. Template
             2. Original image
             3. Overlay of (1) and (2)
             4. Overlay of RT transformed template and (2)
-    
+
         Returns
         -------
         None.
@@ -230,108 +256,136 @@ class ImageStack():
         TODO
         ----
         Add overlay to show transformed sphere centres and bolt holes.
-    
+
         """
-        plt.subplot(2,2,1)
+        plt.subplot(2, 2, 1)
         plt.imshow(self.template8bit, cmap='gray')
         plt.title('Template')
         plt.axis('off')
-       
-        plt.subplot(2,2,2)
+
+        plt.subplot(2, 2, 2)
         plt.imshow(self.target8bit, cmap='gray')
         plt.title('Image')
         plt.axis('off')
-        
-        plt.subplot(2,2,3)
+        if hasattr(self, 'sample_ROIs'):
+            #colormap = np.array((128, 0, 0, 1.0)) # Select RGBA colour
+            combined_ROI_map = np.sum(self.sample_ROIs, axis=0)
+            #RGB_overlay = combined_ROI_map[..., None] * colormap[None, None, :]
+            #plt.imshow(RGB_overlay)
+            lines = outline_mask(combined_ROI_map)
+            for line in lines:
+                plt.plot(line[1], line[0], color='r', alpha=1)
+
+        plt.subplot(2, 2, 3)
         plt.imshow(self.template8bit/2 + self.target8bit/2, cmap='gray')
         plt.title('Image / template overlay')
         plt.axis('off')
-        
-        plt.subplot(2,2,4)
+
+        plt.subplot(2, 2, 4)
         plt.imshow(self.warped_template8bit/2 + self.target8bit/2, cmap='gray')
         plt.title('Image / fitted template overlay')
         plt.axis('off')
-        
+
         plt.tight_layout()
 
+    def order_by(self, att):
+        """Order images by attribute (e.g. EchoTime, InversionTime)."""
+        self.images.sort(key=lambda x: x[att].value.real)
+        
+    def generate_sample_masks(self, coords_yx, fit_coords=True, 
+                              kernel=None):
+        """Create 3D matrix of sample masks."""
+        if kernel is None:
+            kernel = self.SAMPLE_ELEMENT
+        # If transform_coords == True, rotate coordinates
+        if fit_coords:
+            coords_yx = transform_coords(coords_yx, self.warp_matrix,
+                                              input_yx=True, output_yx=True)
+        # Generate 3D array of POIs, where 0th dimension is coordinate number
+        num_coords = np.size(coords_yx, axis=0)
+        self.sample_POIs = np.zeros(
+            (num_coords,
+             self.images[0].pixel_array.shape[0],
+             self.images[0].pixel_array.shape[1]))
+        self.sample_ROIs = np.zeros_like(self.sample_POIs)
+        for i in range(num_coords):
+            self.sample_POIs[i, coords_yx[i][0], coords_yx[i][1]] = 1
+            self.sample_ROIs[i] = scipy.ndimage.filters.convolve(
+                self.sample_POIs[i], kernel)
 
 
 class T1ImageStack(ImageStack):
     """Calculates T1 relaxometry."""
-    
-    def __init__(self, image_slices, template_px, plate_number):
-        super().__init__(image_slices, template_px, plate_number)
-        
-        #sort images by TI
-        
+
+    def __init__(self, image_slices, template_px=None, plate_number=None):
+        super().__init__(image_slices, template_px, plate_number=plate_number)
+
+        self.order_by('InversionTime')
 
 
 class T2ImageStack(ImageStack):
     """Calculates T2 relaxometry."""
-    
-    def __init__(self, image_slices, template_px, plate_number):
-        super().__init__(image_slices, template_px, plate_number)
-        
-        #Sort images by TE
 
+    def __init__(self, image_slices, template_px=None, plate_number=None):
+        super().__init__(image_slices, template_px, plate_number=plate_number)
 
-
-
+        self.order_by('EchoTime')
 
 # Coordinates of centre of spheres in plate 5.
 # Coordinates are in array format (y,x), rather than plt.patches format (x,y)
 plate5_sphere_centres_yx = (
-    (56,95),
-    (62,117),
-    (81,133),
-    (104,134),
-    (124,121),
-    (133,98),
-    (127,75),
-    (109,61),
-    (84,60),
-    (64,72),
-    (80,81),
-    (78,111),
-    (109,113),
-    (110,82))
+    (56, 95),
+    (62, 117),
+    (81, 133),
+    (104, 134),
+    (124, 121),
+    (133, 98),
+    (127, 75),
+    (109, 61),
+    (84, 60),
+    (64, 72),
+    (80, 81),
+    (78, 111),
+    (109, 113),
+    (110, 82))
 
 plate5_bolt_centres_yx = (
-    (52,80),
-    (92,141),
-    (138,85))
+    (52, 80),
+    (92, 141),
+    (138, 85))
 
-plate5_template_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                                    'data', 'relaxometry',
-                                                    'Plate5_T1_signed')
+plate5_template_path = \
+    os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                 'data', 'relaxometry',
+                 'Plate5_T1_signed')
 template_path = plate5_template_path
-
 
 def main(dcm_target_list, template_px, show_plot=True):
 
     # debug-show only do T1
     t1_image_stack = T1ImageStack(dcm_target_list, template_px, plate_number=5)
     t1_image_stack.template_fit()
-    
+    t1_image_stack.generate_sample_masks(plate5_sphere_centres_yx)
+
     if show_plot:
         t1_image_stack.plot_fit()
-    
-    return t1_image_stack # for debbing only
-    
+
+    return t1_image_stack  # for debbing only
+
 
 
 # Code below is for development only and should be deleted before release.
 if __name__ == '__main__':
-    
+
     import os, os.path
-    import logging # better to set up module level logging
+    import logging  # better to set up module level logging
     from pydicom.errors import InvalidDicomError
-    
-    template_px=pydicom.read_file(template_path).pixel_array
-    
+
+    template_px = pydicom.read_file(template_path).pixel_array
+
     # get list of pydicom objects
     target_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                 '..', 'tests', 'data', 'relaxometry', 'T1', 
+                                 '..', 'tests', 'data', 'relaxometry', 'T1',
                                  'site1 20200218', 'plate 5')
     dcm_target_list = []
     (_,_,filenames) = next(os.walk(target_folder)) # get filenames, don't go to subfolders
@@ -340,9 +394,7 @@ if __name__ == '__main__':
             with pydicom.dcmread(os.path.join(target_folder, filename)) as dcm_target:
                 dcm_target_list.append(dcm_target)
         except InvalidDicomError:
-                logging.info(' Skipped non-DICOM file %r',
-                             os.path.join(target_folder, filename))
-                
-    
-    
+            logging.info(' Skipped non-DICOM file %r',
+                         os.path.join(target_folder, filename))
+
     t1_image_stack = main(dcm_target_list, template_px)
