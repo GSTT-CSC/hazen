072fbdace2453ae8904768bdac45868ad3b26a37
Paul Wilson <paulc.wilson@belfasttrust.hscni.net>
2021-04-16 15:55:04 +0100

    HZN-103 Try to fix Bitbucket pipeline failure again

--- tests/test_snr_map.py
+++ tests/test_snr_map.py
@@ -9,7 +9,7 @@ from tests import TEST_DATA_DIR
 
 class TestSnrMap(unittest.TestCase):
 
-    siemens_1 = os.path.join(TEST_DATA_DIR, 'snr', r'tra_250_2meas_1.ima')
+    siemens_1 = os.path.join(TEST_DATA_DIR, 'snr', 'Siemens', 'tra_250_2meas_1.IMA')
     siemens_2 = os.path.join(TEST_DATA_DIR, 'snr', 'tra_250_2meas_1.ima')
     siemens_3 = os.path.join(TEST_DATA_DIR, 'snr', r'tra_250_2meas_1.IMA')
 
@@ -19,22 +19,7 @@ class TestSnrMap(unittest.TestCase):
         dcms = [pydicom.dcmread(self.siemens_1)]
         results = hazen_snr_map.main(dcms, report_path=True)
         np.testing.assert_almost_equal(192.88188017908504,
-            results['snr_map_snr_seFoV250_2meas_slice5mm_tra_repeat_PSN_noDC_2_1_tra_250_2meas_1.ima'])
-
-    def test_snr_value2(self):
-
-        dcms = [pydicom.dcmread(self.siemens_2)]
-        results = hazen_snr_map.main(dcms, report_path=True)
-        np.testing.assert_almost_equal(192.88188017908504,
-            results['snr_map_snr_seFoV250_2meas_slice5mm_tra_repeat_PSN_noDC_2_1_tra_250_2meas_1.ima'])
-
-    def test_snr_value3(self):
-
-        dcms = [pydicom.dcmread(self.siemens_3)]
-        results = hazen_snr_map.main(dcms, report_path=True)
-        np.testing.assert_almost_equal(192.88188017908504,
             results['snr_map_snr_seFoV250_2meas_slice5mm_tra_repeat_PSN_noDC_2_1_tra_250_2meas_1.IMA'])
-
 
 if __name__ == '__main__':
     unittest.main()
