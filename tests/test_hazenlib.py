54e6fb18fbc7148f8924e669ccd927d925e15101
Lucrezia <83493021+Lucrezia-Cester@users.noreply.github.com>
2021-12-08 10:58:44 +0000

    updated hazenlib_init_ tests
    Hi @laurencejackson, I have added the changes you suggested (If my interpretation was correct). I could not add the test_field_of_view with the other tests because the GE manufacturer was not working with the hazenlib function. 
    Also, I could not add the enhanced_dicom = True bit because I do not have a test image which returns true for that condition.

--- tests/test_hazenlib.py
+++ tests/test_hazenlib.py
@@ -1,12 +1,7 @@
-"""
-Tests functions in the hazenlib.__init__.py file
-"""
-
-
-import unittest
 import pydicom
+from tests import TEST_DATA_DIR
 import hazenlib
-import os
+import unittest
 import numpy as np
 from docopt import docopt
 from hazenlib.logger import logger
@@ -14,170 +9,110 @@ from pprint import pprint
 import sys
 import os
 
-
-
-
-from tests import TEST_DATA_DIR
-
+TEST_DICOM = str(TEST_DATA_DIR / 'toshiba' / 'TOSHIBA_TM_MR_DCM_V3_0.dcm')
+TEST_DICOM = pydicom.read_file(TEST_DICOM)
+print(TEST_DICOM.Columns * TEST_DICOM.PixelSpacing[0])
 
 class TestHazenlib(unittest.TestCase):
-    # Data by ImplementationVersionName
-    # all test values are taken from DICOM headers
-    MANUFACTURER = "philips"
-    ROWS = 512
-    COLUMNS = 512
-    TR_CHECK = 500
-    BW = 205.0
-    ENHANCED = False
-    PIX_ARRAY = 1
 
 
+    def test_intit(self):
+        test_dicoms = {'philips': {'file': str(TEST_DATA_DIR / 'resolution' / 'philips' / 'IM-0004-0002.dcm'),
+                                   'MANUFACTURER': 'philips',
+                                   'ROWS': 512,
+                                   'COLUMNS': 512,
+                                   'TR_CHECK': 500,
+                                   'BW': 205.0,
+                                   'ENHANCED': False,
+                                   'PIX_ARRAY': 1,
+                                   'SLICE_THICKNESS':5,
+                                   'PIX_SIZE': [0.48828125, 0.48828125],
+                                   'AVERAGE': 1},
+                        'siemens': {'file': str(TEST_DATA_DIR / 'resolution' / 'eastkent' / '256_sag.IMA'),
+                                    'MANUFACTURER': 'siemens',
+                                    'ROWS' : 256,
+                                    'COLUMNS' : 256,
+                                    'TR_CHECK' :  500,
+                                    'BW' : 130.0,
+                                    'ENHANCED': False,
+                                    'PIX_ARRAY': 1,
+                                    'SLICE_THICKNESS': 5,
+                                    'PIX_SIZE':  [0.9765625, 0.9765625],
+                                     'AVERAGE': 1},
+                        'toshiba': {'file': str(TEST_DATA_DIR / 'toshiba' / 'TOSHIBA_TM_MR_DCM_V3_0.dcm'),
+                                    'MANUFACTURER': 'toshiba',
+                                    'ROWS': 256,
+                                    'COLUMNS': 256,
+                                    'TR_CHECK': 45.0,
+                                    'BW': 244.0,
+                                    'ENHANCED': False,
+                                    'PIX_ARRAY': 1,
+                                    'SLICE_THICKNESS': 6,
+                                    'PIX_SIZE':  [1.0, 1.0],
+                                    'AVERAGE': 1},
+                        'ge': {'file': str(TEST_DATA_DIR / 'ge' / 'ge_eFilm.dcm'),
+                                    'MANUFACTURER': 'ge',
+                                    'ROWS': 256,
+                                    'COLUMNS': 256,
+                                    'TR_CHECK': 1000.0,
+                                    'BW': 156.25,
+                                    'ENHANCED': False,
+                                    'PIX_ARRAY': 1,
+                                    'SLICE_THICKNESS': 5,
+                                    'PIX_SIZE':  [0.625, 0.625],
+                                    'AVERAGE': 1}}
 
 
+        for manufacturer in test_dicoms.keys():
+            with pydicom.read_file(test_dicoms[manufacturer]['file']) as dcm:
+                #first test function
+                assert hazenlib.get_manufacturer(dcm) == test_dicoms[manufacturer]['MANUFACTURER']
+                #second test function
+                rows = hazenlib.get_rows(dcm)
+                assert rows == test_dicoms[manufacturer]['ROWS']
+                #third test function
+                columns = hazenlib.get_columns(dcm)
+                assert columns == test_dicoms[manufacturer]['COLUMNS']
+                #fourth test function
+                TR = hazenlib.get_TR(dcm)
+                assert TR == test_dicoms[manufacturer]['TR_CHECK']
+                #fifth test_function
+                bw = hazenlib.get_bandwidth(dcm)
+                assert bw == test_dicoms[manufacturer]['BW']
+                #sixth test function
+                enhanced = hazenlib.is_enhanced_dicom(dcm)
+                assert enhanced == test_dicoms[manufacturer]['ENHANCED']
+                #seventh test funvtion
+                pix_arr = hazenlib.get_num_of_frames(dcm)
+                assert pix_arr == test_dicoms[manufacturer]['PIX_ARRAY']
+                #eigth test funciton
+                slice_thick = hazenlib.get_slice_thickness(dcm)
+                assert slice_thick == test_dicoms[manufacturer]['SLICE_THICKNESS']
+                #nineth test function
+                avg = hazenlib.get_average(dcm)
+                assert avg == test_dicoms[manufacturer]['AVERAGE']
+                #tenth test function
+                pix_size = hazenlib.get_pixel_size(dcm)
+                pix_size = list(pix_size)
+                self.assertEqual(pix_size,test_dicoms[manufacturer]['PIX_SIZE'])
 
-    def setUp(self):
-        self.file = str(TEST_DATA_DIR / 'resolution' / 'philips' / 'IM-0004-0002.dcm')
-        self.dcm = pydicom.read_file(self.file)
+    def test_fov(self):
+        test_dicoms = {'philips': {'file': str(TEST_DATA_DIR / 'resolution' / 'philips' / 'IM-0004-0002.dcm'),
+                                   'MANUFACTURER': 'philips',
+                                   'FOV': 250.0},
+                       'siemens': {'file': str(TEST_DATA_DIR / 'resolution' / 'eastkent' / '256_sag.IMA'),
+                                   'MANUFACTURER': 'siemens',
+                                   'FOV': 250.0},
+                       'toshiba': {'file': str(TEST_DATA_DIR / 'toshiba' / 'TOSHIBA_TM_MR_DCM_V3_0.dcm'),
+                                   'MANUFACTURER': 'toshiba',
+                                   'FOV': 256.0}}
 
-    def test_get_bandwidth(self):
-        bw = hazenlib.get_bandwidth(self.dcm)
-        assert bw == self.BW
-
-
-
-    def test_get_rows(self):
-        rows = hazenlib.get_rows(self.dcm)
-        assert rows == self.ROWS
-
-    def test_get_columns(self):
-        columns = hazenlib.get_columns(self.dcm)
-        assert columns == self.COLUMNS
-
-    def test_get_TR(self):
-        TR = hazenlib.get_TR(self.dcm)
-        assert TR == self.TR_CHECK
-
-    def test_is_enhanced_dicom(self):
-        enhanced = hazenlib.is_enhanced_dicom(self.dcm)
-        assert enhanced == self.ENHANCED
-
-    def test_get_num_of_frames(self):
-        pix_arr = hazenlib.get_num_of_frames(self.dcm)
-        assert pix_arr == self.PIX_ARRAY
-
-    def test_get_slice_thickness(self):
-        SLICE_THICK = self.dcm.SliceThickness
-        slice_thick = hazenlib.get_slice_thickness(self.dcm)
-        assert slice_thick == SLICE_THICK
-
-    def test_get_pixel_size(self):
-        PIX_SIZE = self.dcm.PixelSpacing
-        PIX_SIZE=tuple(PIX_SIZE)
-        pix_size = hazenlib.get_pixel_size(self.dcm)
-        assert pix_size == PIX_SIZE
-
-    def test_get_average(self):
-        AVG = self.dcm.NumberOfAverages
-        avg = hazenlib.get_average(self.dcm)
-        assert avg == AVG
-
-    def test_get_manufacturer(self):
-        assert hazenlib.get_manufacturer(self.dcm) == self.MANUFACTURER
-
-file = str(TEST_DATA_DIR / 'resolution' / 'philips' / 'IM-0004-0002.dcm')
-dcm = pydicom.read_file(file)
-FOV = dcm.Columns * dcm.PixelSpacing[0]
-fov = hazenlib.get_field_of_view(dcm)
-assert fov == FOV
-
-
-
-
-
-class TestFactorsPhilipsMR531(TestHazenlib):
-    #PHILIPS_MR_53_1
-
-    MANUFACTURER = 'philips'
-    ROWS = 512
-    COLUMNS = 512
-    TR_CHECK = 500
-    BW = 205.0
-
-    def setUp(self):
-        self.file = str(TEST_DATA_DIR / 'resolution' / 'philips' / 'IM-0004-0002.dcm')
-        self.dcm = pydicom.read_file(self.file)
-
-file = str(TEST_DATA_DIR / 'resolution' / 'philips' / 'IM-0004-0002.dcm')
-dcm = pydicom.read_file(file)
-FOV = dcm.Columns * dcm.PixelSpacing[0]
-fov = hazenlib.get_field_of_view(dcm)
-assert fov == FOV
-
-
-
-class TestFactorsSiemensMRVE11C(TestHazenlib):
-    #SIEMENS_MR_VE11C
-    ROWS = 256
-    COLUMNS = 256
-    TR_CHECK = 500
-    BW = 130.0
-    MANUFACTURER = 'siemens'
-
-    def setUp(self):
-        self.file = str(TEST_DATA_DIR / 'resolution' / 'eastkent' / '256_sag.IMA')
-        self.dcm = pydicom.read_file(self.file)
-        FOV = self.dcm.Columns * self.dcm.PixelSpacing[0]
-
-file = str(TEST_DATA_DIR / 'resolution' / 'eastkent' / '256_sag.IMA')
-dcm = pydicom.read_file(file)
-FOV = dcm.Columns * dcm.PixelSpacing[0]
-fov = hazenlib.get_field_of_view(dcm)
-assert fov == FOV
-
-
-class TestFactorsToshibaTMMRDCMV30(TestHazenlib):
-    # TOSHIBA_TM_MR_DCM_V3_0
-    ROWS = 256
-    COLUMNS = 256
-    TR_CHECK = 45.0
-    BW = 244.0
-    MANUFACTURER = 'toshiba'
-
-    def setUp(self):
-        self.file = str(TEST_DATA_DIR / 'toshiba' / 'TOSHIBA_TM_MR_DCM_V3_0.dcm')
-        self.dcm = pydicom.read_file(self.file)
-        FOV = self.dcm.Columns * self.dcm.PixelSpacing[0]
-
-
-file = str(TEST_DATA_DIR / 'toshiba' / 'TOSHIBA_TM_MR_DCM_V3_0.dcm')
-dcm = pydicom.read_file(file)
-FOV = dcm.Columns * dcm.PixelSpacing[0]
-fov = hazenlib.get_field_of_view(dcm)
-assert fov == FOV
-
-
-
-
-class TestFactorsGEeFilm(TestHazenlib):
-    # GE_eFILM
-    ROWS = 256
-    COLUMNS = 256
-    TR_CHECK = 1000.0
-    BW = 156.25
-    MANUFACTURER ='ge'
-
-    def setUp(self):
-        self.file = str(TEST_DATA_DIR / 'ge' / 'ge_eFilm.dcm')
-        self.dcm = pydicom.read_file(self.file)
-
-
-file = str(TEST_DATA_DIR / 'ge' / 'ge_eFilm.dcm')
-dcm = pydicom.read_file(file)
-#FOV = dcm.Columns * dcm.PixelSpacing[0]
-#fov = hazenlib.get_field_of_view(dcm)
-#print(fov)
-
-
+        for manufacturer in test_dicoms.keys():
+            with pydicom.read_file(test_dicoms[manufacturer]['file']) as dcm:
+                # first test function
+                fov = hazenlib.get_field_of_view(dcm)
+                print(fov)
+                assert fov == test_dicoms[manufacturer]['FOV']
 
 
 
@@ -188,15 +123,13 @@ class Test(unittest.TestCase):
         self.dcm = pydicom.read_file(self.file)
         self.dcm = self.dcm.pixel_array
 
-    def test_isupper(self):
+    def test_rescale_to_byte(self):
         test_array = np.array([[1,2], [3,4]])
         TEST_OUT = np.array([[63,127],[191,255]])
         test_array = hazenlib.rescale_to_byte(test_array)
         test_array = test_array.tolist()
         TEST_OUT = TEST_OUT.tolist()
         self.assertListEqual(test_array, TEST_OUT)
-
-
 
 
 
@@ -263,11 +196,9 @@ class TestCliParser(unittest.TestCase):
 
         output = hazenlib.main()
 
-        dict1 = {'Spin Echo_32_2_P4_t1': {'rms_frac_time_difference': 0.13499936644959426}}
+        dict1 = {'Spin Echo_32_2_P4_t1': {'rms_frac_time_difference': 0.13499936644959415}}
         self.assertDictEqual(dict1, output)
 
-if __name__ == "__main__":
-    unittest.main()
 
 
 
