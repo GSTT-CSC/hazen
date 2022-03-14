17422cd773593e1cf368908b77ea3605d9006002
Yassine Azma <yassine.azma@rmh.nhs.uk>
2022-03-14 13:33:33 +0000

    Update acr_geometric_accuracy.py
    Working script which produces lengths for horizontal, vertical and diagonals (slice 5 only) for slices 1 and 5.

--- hazenlib/acr_geometric_accuracy.py
+++ hazenlib/acr_geometric_accuracy.py
@@ -73,6 +73,7 @@ def geo_accuracy_slice1(dcm):
 
 def geo_accuracy_slice5(dcm):
     img = dcm.pixel_array
+    dims = img.shape
     cxy = centroid_com(img)
     thresh_img = img > 0.25 * np.max(img)
     open_img = skimage.morphology.area_opening(thresh_img, area_threshold=500)
@@ -80,9 +81,38 @@ def geo_accuracy_slice5(dcm):
 
     res = dcm.PixelSpacing  # In-plane resolution from metadata
 
-    L = [hori_length(bhull,cxy,res), vert_length(bhull,cxy,res)]
-    rot_matrix_se = rot_matrix(45)
-    print(rot_matrix_se)
+    dist_h, dist_v = [hori_length(bhull,cxy,res), vert_length(bhull,cxy,res)]
+    rot_matrix_sw = rot_matrix(45)
+    rot_matrix_se = rot_matrix(90)
+
+    coords = np.arange(1,dims[1],1)-cxy[0], [cxy[1]]*dims[0]-cxy[1]
+    rot_coords_sw = np.matmul(coords, rot_matrix_sw, dtype=object)
+
+    y_sw = rot_coords_sw[0][0]
+    x_sw = rot_coords_sw[1][0]
+
+    start_sw = (cxy[0] + x_sw[0], cxy[1] + y_sw[0])
+    end_sw = (cxy[0] + x_sw[-1], cxy[1] + y_sw[-1])
+    line_profile_sw = skimage.measure.profile_line(bhull, start_sw, end_sw)
+
+    rot_coords_se = np.matmul(rot_coords_sw, rot_matrix_se, dtype=object)
+
+    y_se = rot_coords_se[0][0]
+    x_se = rot_coords_se[1][0]
+
+    start_se = (cxy[0] + x_se[0], cxy[1] + y_se[0])
+    end_se = (cxy[0] + x_se[-1], cxy[1] + y_se[-1])
+    line_profile_se = skimage.measure.profile_line(bhull, start_se, end_se)
+
+    extent_se = np.nonzero(line_profile_se)[0]
+    extent_sw = np.nonzero(line_profile_sw)[0]
+
+    eff_res = np.sqrt(np.mean(np.square(res)))
+
+    dist_se = (extent_se[-1]-extent_se[0])*eff_res
+    dist_sw = (extent_sw[-1]-extent_sw[0])*eff_res
+
+    L = dist_h, dist_v, dist_se, dist_sw
     return L
 
 
@@ -126,6 +156,8 @@ def main(data: list, report_path=False) -> dict:
         result[f"geometric_accuracy_slice1_vertical_{key}"] = np.round(L[0][1], 2)
         result[f"geometric_accuracy_slice5_horizontal_{key}"] = np.round(L[1][0], 2)
         result[f"geometric_accuracy_slice5_vertical_{key}"] = np.round(L[1][1], 2)
+        result[f"geometric_accuracy_slice5_diagonalse2nw_{key}"] = np.round(L[1][2], 2)
+        result[f"geometric_accuracy_slice5_diagonalsw2ne_{key}"] = np.round(L[1][3], 2)
     except Exception as e:
         print(f"Could not calculate the geometric accuracy for {key} because of : {e}")
         traceback.print_exc(file=sys.stdout)
