9931be626c7514d8fa231a3c6b4d6ae828349abb
Haleema A <Haleema.AlJazzaf@gstt.nhs.uk>
2022-04-12 10:29:04 +0100

    Updates setUp().

--- tests/test_snr.py
+++ tests/test_snr.py
@@ -6,19 +6,20 @@ import pydicom
 import hazenlib.snr
 from tests import TEST_DATA_DIR
 
+
 # Note all SNR tests assume 5mm slice thickness
 class TestSnr(unittest.TestCase):
 
-#SIEMENS MR_VE11C
-# 1.5T
+    # SIEMENS MR_VE11C
+    # 1.5T
 
     SNR_DATA = pathlib.Path(TEST_DATA_DIR / 'snr')
     ORIENTATION = 'Transverse'
 
-    OBJECT_CENTRE = (131, 122) # note these coordinates are (x, y) ie. (COLUMN, ROW)
-    SNR_NORM_FACTOR = 9.761711312090041 # checked manually
-    IMAGE_SMOOTHED_SNR = 1874.81 # this value from MATLAB for tra_250_2meas_1.IMA, single image smoothed, normalised
-    IMAGE_SUBTRACT_SNR = 2130.93 # this value from MATLAB for tra_250_2meas_1.IMA and tra_250_2meas_2.IMA, subtract method, normalised
+    OBJECT_CENTRE = (131, 122)  # note these coordinates are (x, y) ie. (COLUMN, ROW)
+    SNR_NORM_FACTOR = 9.761711312090041  # checked manually
+    IMAGE_SMOOTHED_SNR = 1874.81  # this value from MATLAB for tra_250_2meas_1.IMA, single image smoothed, normalised
+    IMAGE_SUBTRACT_SNR = 2130.93  # this value from MATLAB for tra_250_2meas_1.IMA and tra_250_2meas_2.IMA, subtract method, normalised
 
     # setting +/- 2% range for SNR results
     UPPER_SMOOTHED_SNR = IMAGE_SMOOTHED_SNR * 1.02
@@ -30,6 +31,7 @@ class TestSnr(unittest.TestCase):
     def setUp(self):
         self.test_file = pydicom.read_file(str(self.SNR_DATA / 'Siemens'/'tra_250_2meas_1.IMA'), force=True)
         self.test_file_2 = pydicom.read_file(str(self.SNR_DATA / 'Siemens'/'tra_250_2meas_2.IMA'), force=True)
+        self.test_file_3 = pydicom.read_file(str(self.SNR_DATA / 'Philips'/'Philips_IM-0011-0005.dcm'), force=True)
 
     def test_get_object_centre(self):
         assert hazenlib.snr.get_object_centre(self.test_file) == self.OBJECT_CENTRE
@@ -40,15 +42,16 @@ class TestSnr(unittest.TestCase):
         self.assertTrue(self.LOWER_SUBTRACT_SNR <= val["snr_subtraction_normalised_seFoV250_2meas_slice5mm_tra_repeat_PSN_noDC_2_1"] <= self.UPPER_SUBTRACT_SNR)
 
     def test_SNR_factor(self):
-        SNR_factor=hazenlib.snr.get_normalised_snr_factor(self.test_file)
-        assert(SNR_factor) == self.SNR_NORM_FACTOR
+        SNR_factor = hazenlib.snr.get_normalised_snr_factor(self.test_file)
+        assert SNR_factor == self.SNR_NORM_FACTOR
 
     def test_two_inputs_match(self):
         dcm1 = self.test_file
         dcm2 = self.test_file_2
-        dcm3 = pydicom.read_file(str(self.SNR_DATA / 'Philips'/'Philips_IM-0011-0005.dcm'), force=True)
+        dcm3 = self.test_file_3
         self.assertTrue(hazenlib.snr.two_inputs_match(dcm1, dcm2))
         self.assertFalse(hazenlib.snr.two_inputs_match(dcm1, dcm3))
+
 
 class TestSnrPhilips(TestSnr):
     #PHILIPS_MR_53_1
@@ -72,6 +75,7 @@ class TestSnrPhilips(TestSnr):
     def setUp(self):
         self.test_file = pydicom.read_file(str(self.SNR_DATA / 'Philips' / 'Philips_IM-0011-0005.dcm'), force=True)
         self.test_file_2 = pydicom.read_file(str(self.SNR_DATA / 'Philips' / 'Philips_IM-0011-0006.dcm'), force=True)
+        self.test_file_3 = pydicom.read_file(str(self.SNR_DATA / 'Siemens' / 'tra_250_2meas_1.IMA'), force=True)
 
     def test_image_snr(self):
         val = hazenlib.snr.main(data=[self.test_file, self.test_file_2])
@@ -101,6 +105,7 @@ class TestSnrGE(TestSnr):
     def setUp(self):
         self.test_file = pydicom.read_file(str(self.SNR_DATA / 'GE' / 'IM-0003-0001.dcm'), force=True)
         self.test_file_2 = pydicom.read_file(str(self.SNR_DATA / 'GE' / 'IM-0004-0001.dcm'), force=True)
+        self.test_file_3 = pydicom.read_file(str(self.SNR_DATA / 'Siemens' / 'tra_250_2meas_1.IMA'), force=True)
 
     def test_image_snr(self):
         val = hazenlib.snr.main(data=[self.test_file, self.test_file_2])
@@ -113,6 +118,7 @@ class TestSnrThreshold(TestSnr):
     # Example of shape detection failure
 
     SNR_DATA = pathlib.Path(TEST_DATA_DIR / 'snr_threshold')
+    SNR_DATA_2 = pathlib.Path(TEST_DATA_DIR / 'snr')
 
     OBJECT_CENTRE = (133, 125)
     SNR_NORM_FACTOR = 13.537071812733949  # value taken from Hazen
@@ -129,6 +135,7 @@ class TestSnrThreshold(TestSnr):
     def setUp(self):
         self.test_file = pydicom.read_file(str(self.SNR_DATA / 'VIDA' / 'HC_SNR_SAG_1.dcm'), force=True)
         self.test_file_2 = pydicom.read_file(str(self.SNR_DATA / 'VIDA' / 'HC_SNR_SAG_2.dcm'), force=True)
+        self.test_file_3 = pydicom.read_file(str(self.SNR_DATA_2 / 'Siemens' / 'tra_250_2meas_1.IMA'), force=True)
 
     def test_get_object_centre(self):
         assert hazenlib.snr.get_object_centre(self.test_file) == self.OBJECT_CENTRE
