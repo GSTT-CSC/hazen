7fa76e64f0927faa1138203d13e7072c133404a2
Haris Shuaib <haris.shuaib+github@gmail.com>
2022-07-30 18:07:42 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Haris Shuaib'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.6.0'
+release = '0.6.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -86,7 +86,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
