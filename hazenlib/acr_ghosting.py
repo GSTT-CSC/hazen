3ba7cc09bf55784949b2962735ab3b3bd51bce54
Yassine Azma <yassine.azma@rmh.nhs.uk>
2022-11-14 11:48:47 +0000

    Create acr_ghosting.py
    Adds a description for ghosting script. Test push after hiatus!

---
+++ hazenlib/acr_ghosting.py
@@ -0,0 +1,14 @@
+"""
+ACR Ghosting
+
+Calculates uniformity for slice 7 of the ACR phantom.
+
+This script calculates the percentage signal ghosting in accordance with the ACR Guidance.
+This is done by first defining a large 200cm2 ROI before placing 10cm2 elliptical ROIs outside the phantom along the
+cardinal directions. The results are also visualised.
+
+Created by Yassine Azma
+yassine.azma@rmh.nhs.uk
+
+14/11/2022
+"""
