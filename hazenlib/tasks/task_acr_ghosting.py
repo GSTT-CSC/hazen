d5782222733dfa9ec40ea99252492f71c543ab22
Yassine Azma <yassine.azma@rmh.nhs.uk>
2022-11-18 13:20:46 +0000

    Update task_acr_ghosting.py
    Corrects error in ghosting docstring.

--- hazenlib/tasks/task_acr_ghosting.py
+++ hazenlib/tasks/task_acr_ghosting.py
@@ -1,7 +1,7 @@
 """
 ACR Ghosting
 
-Calculates uniformity for slice 7 of the ACR phantom.
+Calculates percent-signal ghosting for slice 7 of the ACR phantom.
 
 This script calculates the percentage signal ghosting in accordance with the ACR Guidance.
 This is done by first defining a large 200cm2 ROI before placing 10cm2 elliptical ROIs outside the phantom along the
