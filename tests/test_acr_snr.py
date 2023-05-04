16c3f02c5c5b0377112f6aae7ef140afdb9a200c
Yassine Azma <yassine.azma@rmh.nhs.uk>
2023-05-04 12:19:09 +0100

    Refactoring ACR_SNR
    Added ACR_Tools functionality to ACR SNR scripts.

--- tests/test_acr_snr.py
+++ tests/test_acr_snr.py
@@ -4,51 +4,54 @@ import pathlib
 import pydicom
 
 from hazenlib.tasks.acr_snr import ACRSNR
+from hazenlib.acr_tools import ACRTools
 from tests import TEST_DATA_DIR, TEST_REPORT_DIR
 
 
 class TestACRSNRSiemens(unittest.TestCase):
     ACR_SNR_DATA = pathlib.Path(TEST_DATA_DIR / 'acr')
-    centre = [129, 128]
     norm_factor = 9.761711312090041
-    snr = 335.27
-    sub_snr = 76.48
+    snr = 344.15
+    sub_snr = 75.94
 
     def setUp(self):
         self.acr_snr_task = ACRSNR(data_paths=[os.path.join(TEST_DATA_DIR, 'acr')],
                                    report_dir=pathlib.PurePath.joinpath(TEST_REPORT_DIR))
-        self.dcm = pydicom.read_file(os.path.join(TEST_DATA_DIR, 'acr', 'Siemens', '6.dcm'))
-        self.dcm2 = pydicom.read_file(os.path.join(TEST_DATA_DIR, 'acr', 'Siemens2', '7.dcm'))
+        self.acr_snr_task.ACR_obj = [ACRTools(
+            [pydicom.read_file(os.path.join(TEST_DATA_DIR, 'acr', 'Siemens', f'{i}')) for i in
+             os.listdir(os.path.join(TEST_DATA_DIR, 'acr', 'Siemens'))])]
+        self.acr_snr_task.ACR_obj.append(
+            ACRTools([pydicom.read_file(os.path.join(TEST_DATA_DIR, 'acr', 'Siemens2', f'{i}')) for i in
+                      os.listdir(os.path.join(TEST_DATA_DIR, 'acr', 'Siemens2'))]))
 
-    def test_object_centre(self):
-        assert self.acr_snr_task.centroid(self.dcm)[1] == self.centre
+        self.dcm = [i.dcm[6] for i in self.acr_snr_task.ACR_obj]
 
     def test_normalisation_factor(self):
-        SNR_factor = self.acr_snr_task.get_normalised_snr_factor(self.dcm)
+        SNR_factor = self.acr_snr_task.get_normalised_snr_factor(self.dcm[0])
         assert SNR_factor == self.norm_factor
 
     def test_snr_by_smoothing(self):
-        snr, _ = self.acr_snr_task.snr_by_smoothing(self.dcm)
+        snr, _ = self.acr_snr_task.snr_by_smoothing(self.dcm[0])
         assert round(snr, 2) == self.snr
 
     def test_snr_by_subtraction(self):
-        snr, _ = self.acr_snr_task.snr_by_subtraction(self.dcm, self.dcm2)
+        snr, _ = self.acr_snr_task.snr_by_subtraction(self.dcm[0], self.dcm[1])
         assert round(snr, 2) == self.sub_snr
 
 
 class TestACRSNRGE(unittest.TestCase):
     ACR_SNR_DATA = pathlib.Path(TEST_DATA_DIR / 'acr')
-    centre = [253, 256]
     norm_factor = 57.12810400630368
-    snr = 39.97
+    snr = 40.19
 
     def setUp(self):
         self.acr_snr_task = ACRSNR(data_paths=[os.path.join(TEST_DATA_DIR, 'acr')],
                                    report_dir=pathlib.PurePath.joinpath(TEST_REPORT_DIR))
-        self.dcm = pydicom.read_file(os.path.join(TEST_DATA_DIR, 'acr', 'GE', '4.dcm'))
+        self.acr_snr_task.ACR_obj = [ACRTools(
+            [pydicom.read_file(os.path.join(TEST_DATA_DIR, 'acr', 'GE', f'{i}')) for i in
+             os.listdir(os.path.join(TEST_DATA_DIR, 'acr', 'GE'))])]
 
-    def test_object_centre(self):
-        assert self.acr_snr_task.centroid(self.dcm)[1] == self.centre
+        self.dcm = self.acr_snr_task.ACR_obj[0].dcm[6]
 
     def test_normalisation_factor(self):
         SNR_factor = self.acr_snr_task.get_normalised_snr_factor(self.dcm)
