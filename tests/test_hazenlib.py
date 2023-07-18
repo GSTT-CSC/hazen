de70827d10fa72c74621c59818faaed90df5aabb
sophie22 <sophie22@users.noreply.github.com>
2023-07-18 13:33:38 +0100

    directly call the run() function of the task to capture the result for comparison

--- tests/test_hazenlib.py
+++ tests/test_hazenlib.py
@@ -1,14 +1,11 @@
-import pydicom
+import sys
 from tests import TEST_DATA_DIR, TEST_REPORT_DIR
 import unittest
-import numpy as np
-from docopt import docopt
-from hazenlib.logger import logger
-import hazenlib.utils
-from pprint import pprint
-import sys
-import ast
-import os
+import pydicom
+import hazenlib
+from hazenlib.utils import get_dicom_files, is_dicom_file
+from hazenlib.tasks.snr import SNR
+from hazenlib.relaxometry import main as relaxometry_run
 
 
 class TestCliParser(unittest.TestCase):
@@ -46,10 +43,11 @@ class TestCliParser(unittest.TestCase):
 
     def test_snr_measured_slice_width(self):
         path = str(TEST_DATA_DIR / 'snr' / 'GE')
-        sys.argv = ["hazen", "snr", path, "--measured_slice_width", "1"]
+        files = get_dicom_files(path)
+        # sys.argv = ["hazen", "snr", path, "--measured_slice_width", "1"]
+        snr_task = SNR(data_paths=files, report=False)
+        result = snr_task.run(measured_slice_width=1)
 
-        output = hazenlib.main()
-        output_dict = ast.literal_eval(output)
         dict1 = {'snr_subtraction_measured_SNR_SNR_SAG_MEAS1_23_1': 183.97,
                  'snr_subtraction_normalised_SNR_SNR_SAG_MEAS1_23_1': 7593.04,
                  'snr_smoothing_measured_SNR_SNR_SAG_MEAS2_24_1': 183.93,
@@ -57,15 +55,16 @@ class TestCliParser(unittest.TestCase):
                  'snr_smoothing_measured_SNR_SNR_SAG_MEAS1_23_1': 179.94,
                  'snr_smoothing_normalised_SNR_SNR_SAG_MEAS1_23_1': 7426.54}
 
-        self.assertDictEqual(output_dict['SNR_SNR_SAG_MEAS1_23_1'], dict1)
+        self.assertDictEqual(result['SNR_SNR_SAG_MEAS1_23_1'], dict1)
 
     def test_relaxometry(self):
         path = str(TEST_DATA_DIR / 'relaxometry' / 'T1' / 'site3_ge' / 'plate4')
-        sys.argv = ["hazen", "relaxometry", path, "--plate_number", "4", "--calc_t1"]
-
-        output = hazenlib.main()
-        output_dict = ast.literal_eval(output)
+        files = get_dicom_files(path)
+        dicom_objects = [pydicom.read_file(x, force=True) for x in files if is_dicom_file(x)]
+        # sys.argv = ["hazen", "relaxometry", path, "--plate_number", "4", "--calc_t1"]
+        result = relaxometry_run(dicom_objects, plate_number=4,
+         calc_t1=True, calc_t2=False, report_path=False, verbose=False)
 
         dict1 = {'Spin Echo_32_2_P4_t1': {'rms_frac_time_difference': 0.13499936644959437}}
         self.assertAlmostEqual(dict1['Spin Echo_32_2_P4_t1']['rms_frac_time_difference'],
-                               output_dict['Spin Echo_32_2_P4_t1']['rms_frac_time_difference'], 4)
+                               result['Spin Echo_32_2_P4_t1']['rms_frac_time_difference'], 4)
