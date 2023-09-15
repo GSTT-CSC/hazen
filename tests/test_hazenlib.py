35c3eab408b39d5bb018a735b5bdf080d6988a6c
sophie22 <sophie22@users.noreply.github.com>
2023-09-15 13:18:56 +0100

    check updated output dict format and values

--- tests/test_hazenlib.py
+++ tests/test_hazenlib.py
@@ -35,20 +35,34 @@ class TestCliParser(unittest.TestCase):
 
         self.assertEqual(logging.root.level, logging.INFO)
 
-    # def test_snr_measured_slice_width(self):
-    #     path = str(TEST_DATA_DIR / 'snr' / 'GE')
-    #     files = get_dicom_files(path)
-    #     snr_task = SNR(input_data=files, report=False)
-    #     result = snr_task.run(measured_slice_width=1)
+    def test_snr_measured_slice_width(self):
+        path = str(TEST_DATA_DIR / 'snr' / 'GE')
+        files = get_dicom_files(path)
+        snr_task = SNR(input_data=files, report=False)
+        result = snr_task.run(measured_slice_width=5)
 
-    #     dict1 = {'snr_subtraction_measured_SNR_SNR_SAG_MEAS1_23_1': 183.97,
-    #              'snr_subtraction_normalised_SNR_SNR_SAG_MEAS1_23_1': 7593.04,
-    #              'snr_smoothing_measured_SNR_SNR_SAG_MEAS1_23_1': 184.41,
-    #              'snr_smoothing_measured_SNR_SNR_SAG_MEAS2_24_1': 189.38,
-    #              'snr_smoothing_normalised_SNR_SNR_SAG_MEAS1_23_1': 7610.83,
-    #              'snr_smoothing_normalised_SNR_SNR_SAG_MEAS2_24_1': 7816.0}
+        dict1 = {
+            "task": "SNR",
+            "file": ["SNR_SAG_MEAS1_23_1", "SNR_SAG_MEAS2_24_1"],
+            "measurement": {
+                "snr by smoothing": {
+                    "SNR_SAG_MEAS1_23_1": {
+                        "measured": 184.41,
+                        "normalised": 1522.17
+                    },
+                    "SNR_SAG_MEAS2_24_1": {
+                        "measured": 189.38,
+                        "normalised": 1563.2
+                    }
+                },
+                "snr by subtraction": {
+                    "measured": 183.97,
+                    "normalised": 1518.61
+                }
+            }
+        }
 
-    #     self.assertDictEqual(result['SNR_SAG_MEAS1_23_1'], dict1)
+        self.assertDictEqual(result, dict1)
 
     def test_relaxometry(self):
         path = str(TEST_DATA_DIR / 'relaxometry' / 'T1' / 'site3_ge' / 'plate4')
@@ -56,7 +70,11 @@ class TestCliParser(unittest.TestCase):
         relaxometry_task = Relaxometry(input_data=files, report=False)
         result = relaxometry_task.run(calc='T1', plate_number=4, verbose=False)
 
-        dict1 = {'Spin Echo_32_2_P4_t1': {'rms_frac_time_difference': 0.13499936644959437}}
+        dict1 = {
+            "task": "Relaxometry",
+            "file": "Spin_Echo_34_2_4_t1",
+            "measurement": {"rms_frac_time_difference": 0.135}
+        }
         self.assertEqual(dict1.keys(), result.keys())
-        self.assertAlmostEqual(dict1['Spin Echo_32_2_P4_t1']['rms_frac_time_difference'],
-                               result['Spin Echo_32_2_P4_t1']['rms_frac_time_difference'], 4)
+        self.assertAlmostEqual(dict1['measurement']['rms_frac_time_difference'],
+                               result['measurement']['rms_frac_time_difference'], 4)
