b61c64456206d4f8355983b02d1879514b56fdfe
mollybuckley <k2366974@kcl.ac.uk>
2024-01-15 10:24:49 +0000

    Corrections after failed Github tests
    Corrections after some of the tests on Github failed (line too long, too many blank lines)

--- hazenlib/tasks/acr_geometric_accuracy.py
+++ hazenlib/tasks/acr_geometric_accuracy.py
@@ -5,10 +5,13 @@ https://www.acraccreditation.org/-/media/acraccreditation/documents/mri/largepha
 
 Calculates geometric accuracy for slices 1 and 5 of the ACR phantom.
 
-This script calculates the horizontal and vertical lengths of the ACR phantom in Slice 1 in accordance with the ACR Guidance.
-This script calculates the horizontal, vertical and diagonal lengths of the ACR phantom in Slice 5 in accordance with the ACR Guidance.
+This script calculates the horizontal and vertical lengths of the ACR phantom in Slice 1 in accordance with the ACR
+Guidance.
+This script calculates the horizontal, vertical and diagonal lengths of the ACR phantom in Slice 5 in accordance with
+the ACR Guidance.
 The average distance measurement error, maximum distance measurement error and coefficient of variation of all distance
-measurements is reported as recommended by IPEM Report 112, "Quality Control and Artefacts in Magnetic Resonance Imaging".
+measurements is reported as recommended by IPEM Report 112, "Quality Control and Artefacts in Magnetic Resonance
+Imaging".
 
 This is done by first producing a binary mask for each respective slice. Line profiles are drawn with aid of rotation
 matrices around the centre of the test object to determine each respective length. The results are also visualised.
@@ -40,7 +43,6 @@ class ACRGeometricAccuracy(HazenTask):
     def run(self) -> dict:
 
         # Initialise results dictionary
-        #global lengths_1, lengths_5
         results = self.init_result_dict()
         results['file'] = [self.img_desc(self.ACR_obj.dcms[0]), self.img_desc(self.ACR_obj.dcms[4])]
 
@@ -65,7 +67,6 @@ class ACRGeometricAccuracy(HazenTask):
         except Exception as e:
             print(f"Could not calculate the geometric accuracy for {self.img_desc(self.ACR_obj.dcms[4])} because of : {e}")
             traceback.print_exc(file=sys.stdout)
-
 
         L = lengths_1 + lengths_5
 
