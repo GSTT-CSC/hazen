975d00adcce566171a6e6b754338b64f1045e630
sophie22 <sophie22@users.noreply.github.com>
2024-01-25 17:43:27 +0000

    update get_object centre and create get_profile_fraction, remove mode()

--- hazenlib/tasks/uniformity.py
+++ hazenlib/tasks/uniformity.py
@@ -1,31 +1,28 @@
 """
-Uniformity + Ghosting & Distortion
+Uniformity
 
 Calculates uniformity for a single-slice image of a uniform MRI phantom
 
 This script implements the IPEM/MAGNET method of measuring fractional uniformity.
 It also calculates integral uniformity using a 75% area FOV ROI and CoV for the same ROI.
 
-This script also measures Ghosting within a single image of a uniform phantom.
-This follows the guidance from ACR for testing their large phantom.
-
-A simple measurement of distortion is also made by comparing the height and width of the circular phantom.
-
-Created by Neil Heraghty
-neil.heraghty@nhs.net
-
+Created by Neil Heraghty neil.heraghty@nhs.net \n
 14/05/2018
 
+Updated by Sophie Ratkai sophie.ratkai@nhs.net \n
+25/01/2024
 """
 
 import os
 import sys
 import traceback
 import numpy as np
+from scipy import stats
 
-import hazenlib.utils
-import hazenlib.exceptions as exc
+from hazenlib.utils import ShapeDetector, get_image_orientation
 from hazenlib.HazenTask import HazenTask
+from hazenlib.logger import logger
+import hazenlib.exceptions as exc
 
 
 class Uniformity(HazenTask):
@@ -46,7 +43,10 @@ class Uniformity(HazenTask):
             dict: results are returned in a standardised dictionary structure specifying the task name, input DICOM Series Description + SeriesNumber + InstanceNumber, task measurement key-value pairs, optionally path to the generated images for visualisation
         """
         results = self.init_result_dict()
-        results["file"] = self.img_desc(self.single_dcm)
+        img_desc = self.img_desc(self.single_dcm)
+        results["file"] = img_desc
+        print("------------------------------")
+        print(img_desc)
 
         try:
             horizontal_uniformity, vertical_uniformity = self.get_fractional_uniformity(
@@ -57,9 +57,7 @@ class Uniformity(HazenTask):
                 "vertical %": round(vertical_uniformity, 2),
             }
         except Exception as e:
-            print(
-                f"Could not calculate the uniformity for {self.img_desc(self.single_dcm)} because of : {e}"
-            )
+            print(f"Could not calculate the uniformity for {img_desc} because of : {e}")
             traceback.print_exc(file=sys.stdout)
 
         # only return reports if requested
@@ -68,48 +66,16 @@ class Uniformity(HazenTask):
 
         return results
 
-    def mode(self, a, axis=0):
-        """Finds the modal value of an array. From scipy.stats.mode
-
-        Args:
-            a (np.array): _description_
-            axis (int, optional): Axis to calculate mode along. Defaults to 0.
-
-        Returns:
-            most_frequent: the modal value
-            old_counts: the number of times this value was counted (check this)
-        """
-        scores = np.unique(np.ravel(a))  # get ALL unique values
-        test_shape = list(a.shape)
-        test_shape[axis] = 1
-        old_most_frequent = np.zeros(test_shape)
-        old_counts = np.zeros(test_shape)
-        most_frequent = None
-
-        for score in scores:
-            template = a == score
-            counts = np.expand_dims(np.sum(template, axis), axis)
-            most_frequent = np.where(counts > old_counts, score, old_most_frequent)
-            old_counts = np.maximum(counts, old_counts)
-            old_most_frequent = most_frequent
-
-        return most_frequent, old_counts
-
-    def get_object_centre(self, dcm):
+    def get_object_centre(self, arr, orientation):
         """Locate centre coordinates
 
         Args:
             dcm (pydicom.FileDataset): DICOM image object
 
-        Raises:
-            Exception: _description_
-
         Returns:
             tuple: x and y coordinates
         """
-        arr = dcm.pixel_array
-        shape_detector = hazenlib.utils.ShapeDetector(arr=arr)
-        orientation = hazenlib.utils.get_image_orientation(dcm.ImageOrientationPatient)
+        shape_detector = ShapeDetector(arr)
 
         if orientation in ["Sagittal", "Coronal"]:
             # orientation is sagittal to patient
@@ -118,14 +84,31 @@ class Uniformity(HazenTask):
             except exc.ShapeError:
                 raise
 
-        elif orientation == "Transverse":
-            # orientation is axial
+        else:
+            # orientation == "Transverse" or axial
             x, y, r = shape_detector.get_shape("circle")
 
-        else:
-            raise Exception("Direction must be Transverse, Sagittal or Coronal.")
-
         return int(x), int(y)
+
+    def get_profile_fraction(self, roi_array, central_roi_mode, direction):
+        """Calculate fractional value of pixel values within threshold across a profile
+
+        Args:
+            roi_array (np.ndarray): pixel array
+            central_roi_mode (int): modal value within central ROI
+            direction (int): 0 or 1 corresponding to horizontal and vertical axis
+
+        Returns:
+            float: profile fraction
+        """
+        profile_mean = np.mean(roi_array, axis=direction, out=None)
+        and_arr = np.logical_and(
+            (profile_mean > (0.9 * central_roi_mode)),
+            (profile_mean < (1.1 * central_roi_mode)),
+        )
+        profile_count = np.where(and_arr)
+        profile_fraction = len(profile_count[0]) / profile_mean.shape[0]
+        return profile_fraction
 
     def get_fractional_uniformity(self, dcm):
         """Get fractional uniformity
@@ -136,40 +119,35 @@ class Uniformity(HazenTask):
         Returns:
             tuple: values of horizontal and vertical fractional uniformity
         """
+
+        # 1. Determine shape of phantom - depends on orientation
         arr = dcm.pixel_array
-        print(type(dcm))
-        x, y = self.get_object_centre(dcm)
+        orientation = get_image_orientation(dcm.ImageOrientationPatient)
 
-        central_roi = arr[(y - 5) : (y + 5), (x - 5) : (x + 5)].flatten()
-        # Create central 10x10 ROI and measure modal value
+        # Locate phantom centre - based on shape detected
+        x, y = self.get_object_centre(arr, orientation)
+        logger.debug(f"Phantom centre coordinates are {x, y}")
 
-        central_roi_mode, mode_popularity = self.mode(central_roi)
+        # Create central 10x10 ROI and get modal value
+        central_roi = arr[(y - 5) : (y + 5), (x - 5) : (x + 5)]
+        mode_result = stats.mode(central_roi, axis=None, keepdims=False)
+        central_roi_mode = mode_result.mode
+        # print(mode_result.mode)
+        # print(mode_result.count)
 
-        # Create 160-pixel profiles (horizontal and vertical, centred at x,y)
-        horizontal_roi = arr[(y - 5) : (y + 5), (x - 80) : (x + 80)]
-        horizontal_profile = np.mean(horizontal_roi, axis=0)
-        vertical_roi = arr[(y - 80) : (y + 80), (x - 5) : (x + 5)]
-        vertical_profile = np.mean(vertical_roi, axis=1)
-
+        # Create 160x10 pixel profiles (horizontal and vertical, centred at x,y)
         # Count how many elements are within 0.9-1.1 times the modal value
-        horizontal_count = np.where(
-            np.logical_and(
-                (horizontal_profile > (0.9 * central_roi_mode)),
-                (horizontal_profile < (1.1 * central_roi_mode)),
-            )
-        )
-        horizontal_count = len(horizontal_count[0])
-        vertical_count = np.where(
-            np.logical_and(
-                (vertical_profile > (0.9 * central_roi_mode)),
-                (vertical_profile < (1.1 * central_roi_mode)),
-            )
-        )
-        vertical_count = len(vertical_count[0])
-
         # Calculate fractional uniformity
-        fractional_uniformity_horizontal = horizontal_count / 160
-        fractional_uniformity_vertical = vertical_count / 160
+
+        horizontal_roi = arr[(y - 5) : (y + 5), (x - 80) : (x + 80)]
+        vertical_roi = arr[(y - 80) : (y + 80), (x - 5) : (x + 5)]
+
+        fractional_uniformity_horizontal = self.get_profile_fraction(
+            horizontal_roi, central_roi_mode, direction=0
+        )
+        fractional_uniformity_vertical = self.get_profile_fraction(
+            vertical_roi, central_roi_mode, direction=1
+        )
 
         if self.report:
             import matplotlib.pyplot as plt
