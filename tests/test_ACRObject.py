0daad986b14def850038e05d6c3d6049af02cbc3
sophie22 <sophie22@users.noreply.github.com>
2024-02-02 16:49:20 +0000

    add unit test for orthogonal length measurement

--- tests/test_ACRObject.py
+++ tests/test_ACRObject.py
@@ -13,6 +13,10 @@ from tests import TEST_DATA_DIR, TEST_REPORT_DIR
 class TestACRTools(unittest.TestCase):
     centre = (130, 130)
     rotation = -1.0
+    horizontal_distance = 191.40625
+    horizontal_end = (128, 255)
+    vertical_distance = 187.5
+    vertical_end = (255, 128)
 
     def setUp(self):
         self.Siemens_data = [
@@ -24,21 +28,33 @@ class TestACRTools(unittest.TestCase):
         self.img1 = self.ACR_object.slice_stack[0].pixel_array
         self.img7 = self.ACR_object.slice_stack[6].pixel_array
 
-    def test_find_rotation(self):
-        rotation_angle = self.ACR_object.determine_rotation(self.img1)
-        assert self.rotation == np.round(rotation_angle, 1)
-
     def test_find_centre(self):
         phantom_centre, _ = self.ACR_object.find_phantom_center(
             self.img7, self.ACR_object.dx, self.ACR_object.dy
         )
         assert (self.centre == np.round(phantom_centre, 1)).all() == True
 
+    def test_find_rotation(self):
+        rotation_angle = self.ACR_object.determine_rotation(self.img1)
+        assert self.rotation == np.round(rotation_angle, 1)
+
+    def test_measure_orthogonal_lengths(self):
+        mask = self.ACR_object.get_mask_image(self.img1)
+        length_dict = self.ACR_object.measure_orthogonal_lengths(mask, 0)
+        assert self.horizontal_distance == length_dict["Horizontal Distance"]
+        assert self.horizontal_end == length_dict["Horizontal End"]
+        assert self.vertical_distance == length_dict["Vertical Distance"]
+        assert self.vertical_end == length_dict["Vertical End"]
+
 
 # Siemens transverse = axial
 class TestACRToolsTRA(TestACRTools):
     centre = (136, 128)
     rotation = 0.0
+    horizontal_distance = 189.0
+    horizontal_end = (128, 255)
+    vertical_distance = 191.0
+    vertical_end = (255, 136)
 
     def setUp(self):
         self.TRA_data = [
@@ -57,6 +73,10 @@ class TestACRToolsTRA(TestACRTools):
 class TestACRToolsCOR(TestACRTools):
     centre = (128, 128)
     rotation = -1.0
+    horizontal_distance = 190.0
+    horizontal_end = (128, 255)
+    vertical_distance = 191.0
+    vertical_end = (255, 126)
 
     def setUp(self):
         self.COR_data = [
@@ -75,6 +95,10 @@ class TestACRToolsCOR(TestACRTools):
 class TestACRToolsSAG(TestACRTools):
     rotation = -90.0
     centre = (130, 148)
+    horizontal_distance = 190.0
+    horizontal_end = (148, 255)
+    vertical_distance = 189.0
+    vertical_end = (255, 130)
 
     def setUp(self):
         self.SAG_data = [
@@ -93,6 +117,10 @@ class TestACRToolsSAG(TestACRTools):
 class TestACRToolsGE(TestACRTools):
     rotation = 0.0
     centre = (254, 256)
+    horizontal_distance = 190.425
+    horizontal_end = (262, 511)
+    vertical_distance = 188.9016
+    vertical_end = (511, 260)
     test_point = (-60.98, -45.62)
 
     def setUp(self):
