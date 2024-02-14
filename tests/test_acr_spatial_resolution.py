8c24c74300d40e65f2964bfdcd8326cc3c8ad2f4
Sophie Ratkai <sophie22@users.noreply.github.com>
2024-02-14 15:36:11 +0000

    Update test_acr_spatial_resolution.py

--- tests/test_acr_spatial_resolution.py
+++ tests/test_acr_spatial_resolution.py
@@ -79,4 +79,4 @@ class TestACRSpatialResolutionGE(TestACRSpatialResolutionSiemens):
     edge_type = "vertical", "upward"
     edge_loc = [5, 7]
     slope = 0.037
-    MTF50 = (0.5, 1.96)
+    MTF50 = (0.5, 1.85)
