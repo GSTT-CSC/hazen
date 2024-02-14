8c3ad37c9729e59ae51d9d3548cdb0c21375bc41
Sophie Ratkai <sophie22@users.noreply.github.com>
2024-02-14 15:24:48 +0000

    Update test values

--- tests/test_acr_ghosting.py
+++ tests/test_acr_ghosting.py
@@ -11,7 +11,7 @@ from tests import TEST_DATA_DIR, TEST_REPORT_DIR
 
 class TestACRGhostingSiemens(unittest.TestCase):
     ACR_DATA = pathlib.Path(TEST_DATA_DIR / "acr" / "Siemens")
-    psg = 0.035
+    psg = 0.034
 
     def setUp(self):
         input_files = get_dicom_files(self.ACR_DATA)
@@ -37,4 +37,4 @@ class TestACRGhostingSiemens(unittest.TestCase):
 
 class TestACRGhostingGE(TestACRGhostingSiemens):
     ACR_DATA = pathlib.Path(TEST_DATA_DIR / "acr" / "GE")
-    psg = 0.482
+    psg = 0.489
