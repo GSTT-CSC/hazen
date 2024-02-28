abb79e1cfc4a615b2bfc68d253373bb850d64e48
sophie22 <sophie22@users.noreply.github.com>
2024-02-28 10:57:35 +0000

    docstring formatting

--- hazenlib/utils.py
+++ hazenlib/utils.py
@@ -330,20 +330,20 @@ def get_image_orientation(iop):
 
 
 def determine_orientation(dcm_list):
-    """Determine the phantom orientation based on DICOM metadata from a list of DICOM images
+    """Determine the phantom orientation based on DICOM metadata from a list of DICOM images.
 
     Note:
         The ImageOrientationPatient tag is a record of the orientation of the
         imaging volume which contains the phantom. The orientation of the
-        imaging volume MAY NOT align with the true phantom orientation
+        imaging volume MAY NOT align with the true phantom orientation.
 
     Args:
-        dcm_list (list): list of pyDICOM image objects
+        dcm_list (list): list of pyDICOM image objects.
 
     Returns:
         tuple (string, list):
-            "saggital", "coronal", "axial", or "unexpected" orientation \n
-            list of the changing ImagePositionPatient values
+            "saggital", "coronal", "axial", or "unexpected" orientation. \n
+            list of the changing ImagePositionPatient values.
     """
     # for dcm in dcm_list:
     #     print(dcm.InstanceNumber) # unique
