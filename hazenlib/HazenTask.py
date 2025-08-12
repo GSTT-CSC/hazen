3425279310bae1e41159565b7e0bdbe10c770a48
Alex Drysdale <alexander.drysdale@wales.nhs.uk>
2025-08-12 11:49:13 +0100

    style!(hazenlib/tasks): HazenTask forced output of results type
    BREAKING CHANGE: Removed HazenTask accepting arbitrary keyword arguments
    Related to #25

--- hazenlib/HazenTask.py
+++ hazenlib/HazenTask.py
@@ -1,64 +1,80 @@
-"""
-HazenTask.py
-"""
-import os
-import pathlib
+"""Base class for Hazen tasks."""
+
+# Typing imports
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+    import pydicom
+
+# Python imports
+from pathlib import Path
+
+# Module imports
 from pydicom import dcmread
 
+# Local imports
 from hazenlib.logger import logger
+from hazenlib.types import Result
 
 
 class HazenTask:
-    """Base class for performing tasks on image sets"""
+    """Base class for performing tasks on image sets."""
 
     def __init__(
-        self, input_data: list, report: bool = False, report_dir=None, **kwargs
-    ):
-        """Initialise a HazenTask instance
+        self,
+        input_data: Sequence,
+        *,
+        report: bool = False,
+        report_dir: str | Path | None = None,
+    ) -> None:
+        """Initialise a HazenTask instance.
 
         Args:
-            input_data (list): list of filepaths to DICOM images
-            report (bool, optional): Whether to create measurement visualisation diagrams. Defaults to False.
-            report_dir (string, optional): Path to output report images. Defaults to None.
+            input_data : list of filepaths to DICOM images
+            report : Whether to create measurement visualisation diagrams.
+                     Defaults to False.
+            report_dir : Path to output report images. Defaults to None.
+
         """
         data_paths = sorted(input_data)
         self.dcm_list = [dcmread(dicom) for dicom in data_paths]
         self.report: bool = report
-        if report_dir is not None:
-            self.report_path = os.path.join(str(report_dir), type(self).__name__)
-        else:
-            self.report_path = os.path.join(
-                os.getcwd(), "report_image", type(self).__name__
-            )
-        if report:
-            pathlib.Path(self.report_path).mkdir(parents=True, exist_ok=True)
-        else:
-            pass
-        self.report_files = []
+        self.report_path = (
+            Path().cwd() / "report_image" / type(self).__name__
+            if report_dir is None
+            else Path(report_dir) / type(self).__name__
+        )
 
-    def init_result_dict(self) -> dict:
-        """Initialise the dictionary that holds measurement results and input description
+        self.report_path.mkdir(parents=True, exist_ok=True)
+        self.report_files: Sequence[str] = []
 
-        Returns:
-            dict: holds measurement results and task input description
+    def init_result_dict(self) -> Result:
+        """Initialise measurement results holder and input description.
+
+        Returns
+            d : holds measurement results and task input description.
+
         """
-        result_dict = {
-            "task": f"{type(self).__name__}",
-            "file": None,
-            "measurement": {},
-        }
-        return result_dict
+        return Result(task=type(self).__name__)
 
-    def img_desc(self, dcm, properties=None) -> str:
-        """Obtain values from the DICOM header to identify input series
+    def img_desc(
+        self, dcm: pydicom.Dataset, properties: Sequence | None = None,
+    ) -> str:
+        """Obtain values from the DICOM header to identify input series.
 
         Args:
-            dcm (pydicom.Dataset): DICOM image object
-            properties (list, optional): list of DICOM header field names supported by pydicom
-                that shuld be used to generate sereis identifier. Defaults to None.
+            dcm : DICOM image object
+            properties : list of DICOM header field names supported by pydicom
+                that shuld be used to generate sereis identifier.
+                Defaults to None.
 
         Returns:
             str: contatenation of the specified DICOM header property values
+
         """
         if properties is None:
             properties = ["SeriesDescription", "SeriesNumber", "InstanceNumber"]
@@ -66,11 +82,11 @@ class HazenTask:
             metadata = [str(dcm.get(field)) for field in properties]
         except KeyError:
             logger.warning(
-                f"Could not find one or more of the following properties: {properties}"
+                f"Could not find one or more of the following properties: {properties}",
             )
             metadata = [
                 str(dcm.get(field)) for field in ["SeriesDescription", "SeriesNumber"]
             ]
 
-        img_desc = "_".join(metadata).replace(" ", "_")
-        return img_desc
+        return "_".join(metadata).replace(" ", "_")
+
