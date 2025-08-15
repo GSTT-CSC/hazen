12c178c38f4fafc1e604ef0cef5d8646ae9a3712
Alex Drysdale <alexander.drysdale@wales.nhs.uk>
2025-08-15 08:49:39 +0100

    fix(__init__): Call .to_json rather than json serialise Result object
    Related to #25

--- hazenlib/__init__.py
+++ hazenlib/__init__.py
@@ -190,7 +190,7 @@ def main():
             task = init_task(selected_task, files, report, report_dir, verbose=verbose)
             result = task.run()
 
-    result_string = json.dumps(result, indent=2)
+    result_string result.to_json()
     print(result_string)
 
 
